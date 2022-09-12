88736681f85040200bb1bbc35b991d3e7dec46bf
Arshya Garg <80321417+arshya-garg@users.noreply.github.com>
2022-09-12 12:42:47 +0530

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+    queue<string> q;
+    q.push("a");
+    q.push("b");
+    q.push("c");
+    q.front();
+    q.pop();
+    q.size();
+    q.empty();
+    
+}
