6162bebe8606bf204feb006baf071f3e17a25c93
Arshya Garg <80321417+arshya-garg@users.noreply.github.com>
2022-09-12 12:42:24 +0530

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    stack<string> s;
+    s.push("love");
+    s.push("babbar");
+    s.push("kumar"); 
+    cout<<s.top()<<endl;
+    s.size();
+    s.empty();
+    
+    
+}
